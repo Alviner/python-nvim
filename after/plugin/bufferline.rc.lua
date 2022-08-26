b01464f45a8db8a73c0e4edc001e1c7ea91b6ee0
Ivan Sitkin <alvinera@yandex.ru>
2022-08-26 21:19:53 +0300

    first commit

--- after/plugin/bufferline.rc.lua
+++ after/plugin/bufferline.rc.lua
@@ -3,12 +3,21 @@ local status, bufferline = pcall(require, 'bufferline')
 if (not status) then return end
 
 bufferline.setup {
-    options = {
-        separator_style = 'thin',
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-        color_icons = true
-    }
+  options = {
+    separator_style = 'thin',
+    show_buffer_close_icons = false,
+    show_close_icon = false,
+    color_icons = true,
+    indicator = {
+      style = 'icon',
+    },
+    diagnostics = 'coc',
+    diagnostics_update_in_insert = true,
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      local icon = level:match("error") and " " or " "
+      return " " .. icon .. count
+    end
+  }
 }
 
 vim.api.nvim_set_keymap('n', '<Tab>', '<cmd>BufferLineCycleNext<cr>', {})
