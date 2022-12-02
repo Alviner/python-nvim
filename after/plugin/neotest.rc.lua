1b5045d94f5d005cd4ea87d3da347a881ac4bc03
Ivan Sitkin <alvinera@yandex.ru>
2022-12-02 14:46:24 +0300

    feat: neotest init config

---
+++ after/plugin/neotest.rc.lua
@@ -0,0 +1,29 @@
+local status, neotest = pcall(require, 'neotest')
+if (not status) then return end
+
+neotest.setup {
+  adapters = {
+  require("neotest-python")({
+      dap = { justMyCode = false },
+      args = { "-vv", "--log-level", "DEBUG"},
+      runner = 'pytest',
+    }),
+  },
+  output = {
+    open_on_run = true,
+  },
+}
+
+local keymap = vim.api.nvim_set_keymap
+keymap('n', '<leader>t', "<cmd>lua require'neotest'.run.run()<cr>", {})
+keymap('n', '<leader>T', "<cmd>lua require'neotest'.run.run(vim.fn.expand('%s'))<cr>", {})
+keymap('n', '<leader>d', "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", {})
+
+local dap = require('dap')
+dap.adapters.python = {
+  type = 'executable',
+  command = 'python',
+  args = { '-m', 'debugpy.adapter' },
+}
+
+keymap('n', '<F9>', "<cmd>lua require'dap'.toggle_breakpoint()<cr>", {})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -15,7 +15,7 @@ packer.startup(function(use)
     }
     use {
         'nvim-lualine/lualine.nvim', -- StatusLine
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+        requires = { 'kyazdani42/nvim-web-devicons'}
     }
     use {
         'neoclide/coc.nvim',
@@ -28,13 +28,13 @@ packer.startup(function(use)
     }
     use {
         'kyazdani42/nvim-tree.lua',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+        requires = { 'kyazdani42/nvim-web-devicons'},
         tag = 'nightly',
     }
     use {
         'akinsho/bufferline.nvim',
         tag = "v2.*",
-        requires = {'kyazdani42/nvim-web-devicons', opt = true }
+        requires = {'kyazdani42/nvim-web-devicons'}
     }
     use 'tpope/vim-obsession'
     use 'tpope/vim-surround'
@@ -46,4 +46,16 @@ packer.startup(function(use)
     use 'cappyzawa/trim.nvim'
     use 'norcalli/nvim-colorizer.lua'
     use 'lewis6991/gitsigns.nvim'
+    use {
+      'nvim-neotest/neotest',
+      requires = {
+        'mfussenegger/nvim-dap',
+        'mfussenegger/nvim-dap-python',
+        'nvim-lua/plenary.nvim',
+        'nvim-treesitter/nvim-treesitter',
+        'antoinemadec/FixCursorHold.nvim',
+        'nvim-neotest/neotest-plenary',
+        'nvim-neotest/neotest-python'
+      }
+    }
 end)
